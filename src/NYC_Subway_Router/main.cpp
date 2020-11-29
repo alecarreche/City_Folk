907dd7da5541470619c675f6bc1818197f85824d
Alec Arreche <alec.arreche@me.com>
2020-11-28 19:36:17 -0500

    RNG

--- src/NYC_Subway_Router/main.cpp
+++ src/NYC_Subway_Router/main.cpp
@@ -1,7 +1,12 @@
 
 #include <fstream>
 #include "graph.cpp"
+#include <random>
+#include <iostream>
+#include <iomanip>
 using namespace std;
+
+vector<pair<double, double>> generateRandomLoc(int n);
 
 int main() {
     Graph g;
@@ -9,4 +14,23 @@ int main() {
     g.importEdges("data/edges.csv");
 
     return 0;
+}
+
+vector<pair<double, double>> generateRandomLoc(int n, double lowerLat, double upperLat, 
+                                                double lowerLong, double upperLong) {
+
+    vector<pair<double, double>> result;
+    uniform_real_distribution<double> LatUnif(lowerLat, upperLat);
+    uniform_real_distribution<double> LongUnif(lowerLong, upperLong);
+    default_random_engine r;
+
+    for (int i = 0; i < n; i++) {
+        double lat = LatUnif(r);
+        double longit = LongUnif(r);
+
+        result.push_back(make_pair(lat, longit));
+    }
+
+    return result;
+
 }
